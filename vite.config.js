6156ce42e814b998365372aa2309206e36e3a0f9
divyanshu <divyanshubisht5734@gmail.com>
2025-01-25 20:48:36 +0530

    remove base property from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/Empolyee-Mangement-system-",
+  // Remove or update the base property
+  // base: "/Empolyee-Mangement-system-",
 })
