821c009e8a47133f7384a1818c33bf5a645b444c
divyanshu <divyanshubisht5734@gmail.com>
2025-01-25 21:07:25 +0530

    update

--- src/components/tasklist/Tasklist.jsx
+++ src/components/tasklist/Tasklist.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 import Accepttask from './Accepttask'
-import Completetask from './completetask'
-import Failedtask from './failedtask'
-import Newtask from './newtask'
+import Completetask from './Completetask'
+import Failedtask from './Failedtask'
+import Newtask from './Newtask'
 
 
-const Tasklist = ({data}) => {
+const Tasklist = ({ data }) => {
 
-    
+
     return (
 
         <>
             <div id='tasklist' className='h-80 w-full gap-5 flex itmes-center justify-start overflow-x-auto flex-nowrap ml-5'>
 
-               {data.tasks.map((elem , idx)=>{
-                     
-                     if(elem.active){
-                           return <Accepttask  key={idx} data={elem}/>
-                     }
-                     if(elem.new_task){
-                           return <Newtask  key={idx} data={elem}/>
-                     }
+                {data.tasks.map((elem, idx) => {
 
-                     if(elem.completed_task){
-                          return <Completetask  key={idx} data={elem}/>
-                     }
-                     
-                     if(elem.failed){
-                         return <Failedtask key={idx} data={elem}/>
-                     }
-               })}
+                    if (elem.active) {
+                        return <Accepttask key={idx} data={elem} />
+                    }
+                    if (elem.new_task) {
+                        return <Newtask key={idx} data={elem} />
+                    }
+
+                    if (elem.completed_task) {
+                        return <Completetask key={idx} data={elem} />
+                    }
+
+                    if (elem.failed) {
+                        return <Failedtask key={idx} data={elem} />
+                    }
+                })}
 
             </div>
         </>
